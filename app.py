d79f6dc751209a9f026e08d4dbf658acf7f1de3c
arnav <arnavjainx@gmail.com>
2024-03-18 12:35:02 -0700

    added web app and corrected index html

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
-#import sqlite3
+# import sqlite3
+
 
 app = Flask(__name__)
 
-DATABASE = 'food_rescue.db'
+DATABASE = 'community_pantry'
 
 def get_db():
     db = sqlite3.connect(DATABASE)
@@ -17,3 +18,33 @@ def close_db(error):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_food():
+    if request.method == 'POST':
+        food_name = request.form['food_name']
+        quantity = request.form['quantity']
+        description = request.form['description']
+        pickup_address = request.form['pickup_address']
+        contact_number = request.form['contact_number']
+  
+        # Insert the data into the database
+        # db = get_db()
+        # db.execute('INSERT INTO Pantry (FoodName, Quantity, Description, ContactNumber, PickupAddress) VALUES (?, ?, ?, ?, ?)', (food_name, quantity, description, pickup_address, contact_number))
+        # db.commit()
+        # db.close()
+        
+        return redirect(url_for('index'))
+    
+    return render_template('add_food.html')
+
+@app.route('/availableFood')
+def available_food():
+    # db = get_db()
+    # sqlite_select_query = """SELECT * from Pantry"""
+    # db.execute(sqlite_select_query)
+    # food_items = db.fetchall()    
+    return render_template('available_food.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
